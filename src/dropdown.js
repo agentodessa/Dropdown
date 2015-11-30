777260705f99a157b6c46ca1f0a67506a84e2e9f
agentodessa <agentodessa2011@gmail.com>
2015-11-30 10:55:36 +0200

    added methods

--- src/dropdown.js
+++ src/dropdown.js
@@ -2,54 +2,90 @@
 	'use strict';
 	$.fn.dropdown = function (parameters) {
 		console.log(parameters);
-		$.extend(true, {}, $.fn.dropdown.settings, parameters);
+		console.log($(this));
+		var $original = $(this);
+		var moduleSelector = $original.selector || '';
+
+
+
+
+		var settings = $.extend(true, {}, $.fn.dropdown.settings, parameters);
+
+
+
+
+
+		var selector = settings.selector;
+		var module;
+
+		var select = $(selector);
+		console.log(select);
+
+		module = {
+			init: function () {
+
+			},
+			destroy: function () {
+
+			},
+			open: function () {
+
+			},
+			close: function () {
+
+			}
+		};
+
+		$.fn.dropdown.settings = {
+
+			onChange: function (value, text, $selected) {
+			},
+			selector: {
+				dropdown: '.select',
+				icon: '.icon',
+				item: '.item',
+				label: '.label'
+			},
+			className: {
+				active: 'active',
+				visible: 'visible'
+			}
+		};
+
+		$.fn.dropdown.settings.templates = {
+			dropdown: function (select) {
+				console.log(select)
+				var values = select.values || {},
+					html = ''
+					;
+				html += '<i class="dropdown icon"></i>';
+				html += '<div class="text"></div>';
+				html += '<div class="menu">';
+				$.each(select.values, function (index, option) {
+					html += (option.disabled)
+						? '<div class="disabled item" data-value="' + option.value + '">' + option.name + '</div>'
+						: '<div class="item" data-value="' + option.value + '">' + option.name + '</div>'
+					;
+				});
+				html += '</div>';
+				return html;
+			}
+		}
+		module.init();
 	};
 
-
-
-
-
-
-
-	$.fn.dropdown.settings = {
-		onChange: function (value, text, $selected) {
-		},
-		dropdown: function (select) {
-			console.log(select)
-			var
-				placeholder = select.placeholder || false,
-				values = select.values || {},
-				html = ''
-				;
-			html += '<i class="dropdown icon"></i>';
-			if (select.placeholder) {
-				html += '<div class="default text">' + placeholder + '</div>';
-			}
-			else {
-				html += '<div class="text"></div>';
-			}
-			html += '<div class="menu">';
-			$.each(select.values, function (index, option) {
-				html += (option.disabled)
-					? '<div class="disabled item" data-value="' + option.value + '">' + option.name + '</div>'
-					: '<div class="item" data-value="' + option.value + '">' + option.name + '</div>'
-				;
-			});
-			html += '</div>';
-			return html;
-		},
-		selector : {
-			dropdown     : '.select',
-			icon         : '> .select.icon',
-			item         : '.item',
-			label        : '> .label'
-		},
-		className : {
-			active      : 'active',
-			visible     : 'visible'
-		}
-	}
-
-
+	////TODO
+	//(function (name, context, definition) {
+	//	if (typeof define === 'function' && define.amd) {
+	//		define(definition);
+	//	}
+	//	else if (typeof module !== 'undefined' && module.exports) {
+	//		module.exports = definition();
+	//	}
+	//	else {
+	//		context[name] = definition();
+	//	}
+	//})
 })
+
 (jQuery);
